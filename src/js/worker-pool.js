76d99f8435e7b16c19f76701daf1930ba7209b3a
Jalal <jalal.chaabane@epitech.eu>
2014-03-30 22:03:22 +0200

    Update worker pool

--- src/js/worker-pool.js
+++ src/js/worker-pool.js
@@ -2,11 +2,39 @@
  * Created by jalal on 30/03/14.
  */
 
-Async.WorkerPool = (function()
+Async.WorkerPool = (function(nbWorker)
 {
-    var workers = [];
+    var _workers = [];
+
+    if (nbWorker === 0)
+    {
+        throw {msg : 'Cannot create worker pool with 0 thread'};
+    }
+    for (var i = 0; i < nbWorker; i++)
+    {
+        _workers.push(new Async.Worker());
+    }
+
+    var getWorker = function()
+    {
+        var lessWorker = _workers[0];
+
+        _workers.forEach(function(worker)
+        {
+           if (worker.getNbTask() < lessWorker.getNbTask())
+           {
+               lessWorker = worker;
+           }
+        });
+        return lessWorker;
+    };
 
     return {
+        post : function(task)
+        {
+            var worker = getWorker();
 
+            return worker.post(task);
+        }
     };
 });
